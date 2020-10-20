d7e9a73f7551035317b41f9236264121abb24bfd
Keren Koshman <53585467+kerenkoshman@users.noreply.github.com>
2020-10-20 23:16:42 +0300

    fixes

--- logic/flights.js
+++ logic/flights.js
@@ -1,13 +1,27 @@
 function Flights() {
   function calculateNumberOfFlights(passengers, capacity) {
-    let numberOfFlights;
-    if (passengers % capacity == 0) {
-      numberOfFlights = passengers / capacity;
-    } else {
-      numberOfFlights = Math.floor(passengers / capacity) + 1;
+    let flights;
+
+    if (passengers < 0 || !Number.isInteger(Number(passengers))) {
+      throw new Error(
+        "The number of passengers must be a positive integer value"
+      );
     }
-    return { calculateNumberOfFlights };
+
+    if (capacity < 0 || !Number.isInteger(Number(capacity))) {
+      throw new Error(
+        "The capacity of the flight must be a positive integer value"
+      );
+    }
+
+    if (passengers % capacity == 0) {
+      flights = passengers / capacity;
+    } else {
+      flights = Math.floor(passengers / capacity) + 1;
+    }
+    return flights;
   }
+  return { calculateNumberOfFlights };
 }
 
 module.exports = Flights();
