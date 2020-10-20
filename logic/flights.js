d9484962034be6bfbbe5c9d51bf950ad737d00e7
Keren Koshman <53585467+kerenkoshman@users.noreply.github.com>
2020-10-20 23:09:54 +0300

    added calculateNumberofFlights

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,13 @@
 function Flights() {
-
+  function calculateNumberOfFlights(passengers, capacity) {
+    let numberOfFlights;
+    if (passengers % capacity == 0) {
+      numberOfFlights = passengers / capacity;
+    } else {
+      numberOfFlights = Math.floor(passengers / capacity) + 1;
+    }
+    return { calculateNumberOfFlights };
+  }
 }
 
+module.exports = Flights();
